28cac71c90afb31eb1961abc577737706bb7f437
Maciej Ciechanowski <ciechan92@gmail.com>
2022-05-18 00:58:00 +0200

    add process of guessing the number

--- src/pl/java/github/Main.java
+++ src/pl/java/github/Main.java
@@ -9,17 +9,33 @@ package pl.java.github;
  */
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         int rangeNumbers = 100;
-        System.out.println(getRandomNumber(rangeNumbers));
+        System.out.println(processOfGuessingTheNumber(getRandomNumber(rangeNumbers)));
 
     }
 
     static int getRandomNumber (int rangeNumbers){
         Random random = new Random();
         return random.nextInt(100) + 1;
+    }
+
+    static String processOfGuessingTheNumber(int randomNumber){
+        Scanner scanner = new Scanner(System.in);
+        int numberEnter = 0;
+        int numberOfTries = 0;
+
+        do {
+            numberEnter = scanner.nextInt();
+            numberOfTries += 1;
+
+        }
+        while (numberEnter != randomNumber);
+
+        return "You guess";
     }
 
 }
